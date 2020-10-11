1fff99ebdac46d6986e0ae0623960128dcf14fa4
Alun Turner <alun.turner@googlemail.com>
2020-10-11 16:19:02 +0100

    complete navbar component, home route

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { BottomNavigationAction, BottomNavigation } from "@material-ui/core";
+import GroupIcon from "@material-ui/icons/Group";
+import PersonIcon from "@material-ui/icons/Person";
+import HomeIcon from "@material-ui/icons/Home";
+import SettingsIcon from "@material-ui/icons/Settings";
+
+function Navbar() {
+  const [value, setValue] = useState("home");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={handleChange}
+      style={{ backgroundColor: "white" }}
+    >
+      <BottomNavigationAction label="Home" value="home" icon={<HomeIcon />} />
+      <BottomNavigationAction
+        label="Personal Stats"
+        value="person"
+        icon={<PersonIcon />}
+      />
+      <BottomNavigationAction
+        label="Group Stats"
+        value="groupStats"
+        icon={<GroupIcon />}
+      />
+      <BottomNavigationAction
+        label="Settings"
+        value="settings"
+        icon={<SettingsIcon />}
+      />
+    </BottomNavigation>
+  );
+}
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Button, Box, TextField } from "@material-ui/core";
+import Navbar from "../components/Navbar";
 
 function Home() {
-  return <div>this is the Home page</div>;
+  return (
+    <Box
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "50%",
+        alignItems: "center",
+        margin: "10vh auto",
+        border: "2px solid grey",
+        padding: "5vh",
+      }}
+    >
+      <Button variant="contained" color="primary">
+        Sleepy Time
+      </Button>
+      <TextField
+        label="Set Time"
+        type="time"
+        defaultValue="07:30"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        inputProps={{
+          step: 300, // 5 min...this doesn't seem to be working as expected
+        }}
+      />
+      <Button variant="contained" color="primary">
+        Undo
+      </Button>
+      <Navbar />
+    </Box>
+  );
 }
 
 export default Home;
