4f67c5637904de15afb556f33dd7f6880662e91c
Alun Turner <alun.turner@googlemail.com>
2020-10-25 16:21:32 +0000

    created Headline and Sleep components

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Box } from '@material-ui/core';
+import Navbar from '../components/Navbar';
+import Headline from '../components/Headline';
+import Sleep from '../components/Sleep';
 
 function Stats() {
-  return <div>this is the stats page</div>;
+  return (
+    <Box
+    style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "50%",
+        alignItems: "center",
+        margin: "10vh auto",
+        border: "2px solid grey",
+        padding: "5vh",
+      }}
+    >
+      <Headline />
+      <Sleep />
+      <Navbar />
+    </Box>
+  );
 }
 
 export default Stats;
