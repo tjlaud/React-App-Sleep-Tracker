5ed48bb352ac3c909edcb1f56dc861e99feda9fd
Alun Turner <alun.turner@googlemail.com>
2020-11-01 16:25:54 +0000

    added admin to data.js

--- src/data/index.js
+++ src/data/index.js
@@ -4,13 +4,25 @@ export const user = {
   email: "Nathan@bar.com",
   password: "barg1!",
   isAdmin: false,
-  dets: ["bnjkabsfio23", "asdghu189", "asdhfi01023"],
+  dets: ["bnjkabsfio23", "asdhfi01023"],
+  createdAt: new Date(0),
+  sleepData: [],
+};
+
+export const admin = {
+  user_id: "123578s80",
+  userName: "ClemFandango",
+  email: "canyou@hearme.com",
+  password: "Clem",
+  isAdmin: true,
+  dets: ["asdghu189"],
   createdAt: new Date(0),
   sleepData: [],
 };
 
 for (let i = 1000; i < 10000; i += 1000) {
   user.sleepData.push({ wentToSleep: new Date(i), wokeUp: new Date(i + 500) });
+  admin.sleepData.push({ wentToSleep: new Date(i), wokeUp: new Date(i + 500) });
 }
 
 export const dets = [
@@ -24,7 +36,7 @@ export const dets = [
   {
     det_id: "asdghu189",
     detName: "20-2",
-    createdBy: "Clem",
+    createdBy: "ClemFandango",
     users: ["248yr38hqfh8"],
     createdAt: new Date(0),
   },
