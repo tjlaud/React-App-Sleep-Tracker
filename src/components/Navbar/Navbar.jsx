cc050441951b0cd9752dd6d36403e7ec197a2962
tjlaud <tjlaud@hotmail.com>
2021-01-23 14:34:12 +0000

    navbar css started

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "@reach/router";
-import { BottomNavigationAction, BottomNavigation } from "@material-ui/core";
+import {
+  BottomNavigationAction,
+  BottomNavigation,
+  Box,
+} from "@material-ui/core";
 import GroupIcon from "@material-ui/icons/Group";
 import PersonIcon from "@material-ui/icons/Person";
 import HomeIcon from "@material-ui/icons/Home";
@@ -25,44 +29,46 @@ function Navbar() {
   // };
 
   return (
-    <BottomNavigation
-      class="Navbar"
-      value={value}
-      style={{ backgroundColor: "white" }}
-    >
-      <BottomNavigationAction
-        label="Home"
-        value=""
-        icon={<HomeIcon />}
-        onClick={() => {
-          navigate("/");
-        }}
-      />
-      <BottomNavigationAction
-        label="Personal"
-        value="stats"
-        icon={<PersonIcon />}
-        onClick={() => {
-          navigate("/stats");
-        }}
-      />
-      <BottomNavigationAction
-        label="Group"
-        value="summary"
-        icon={<GroupIcon />}
-        onClick={() => {
-          navigate("/summary");
-        }}
-      />
-      <BottomNavigationAction
-        label="Settings"
-        value="settings"
-        icon={<SettingsIcon />}
-        onClick={() => {
-          navigate("/settings");
-        }}
-      />
-    </BottomNavigation>
+    <Box className="navBox">
+      <BottomNavigation
+        className="Navbar"
+        value={value}
+        style={{ backgroundColor: "white" }}
+      >
+        <BottomNavigationAction
+          label="Home"
+          value=""
+          icon={<HomeIcon />}
+          onClick={() => {
+            navigate("/");
+          }}
+        />
+        <BottomNavigationAction
+          label="Personal"
+          value="stats"
+          icon={<PersonIcon />}
+          onClick={() => {
+            navigate("/stats");
+          }}
+        />
+        <BottomNavigationAction
+          label="Group"
+          value="summary"
+          icon={<GroupIcon />}
+          onClick={() => {
+            navigate("/summary");
+          }}
+        />
+        <BottomNavigationAction
+          label="Settings"
+          value="settings"
+          icon={<SettingsIcon />}
+          onClick={() => {
+            navigate("/settings");
+          }}
+        />
+      </BottomNavigation>
+    </Box>
   );
 }
 
