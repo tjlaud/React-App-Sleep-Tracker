672b53e49235728206b6e357347ef64c6eede288
tjlaud <tjlaud@hotmail.com>
2021-02-18 00:31:53 +0300

    removed some unecessary code

--- src/components/Headline/Headline.jsx
+++ src/components/Headline/Headline.jsx
@@ -1,20 +1,9 @@
 import React from "react";
-// import { Box } from "@material-ui/core";
 import "./Headline.scss";
 
 function Headline() {
   return (
-    <div
-      className="headlineBox"
-      // style={{
-      //   width: "calc(100% + 10vh)",
-      //   height: "10vh",
-      //   backgroundColor: "lightblue",
-      //   position: "relative",
-      //   top: "-5vh",
-      //   color: "white",
-      // }}
-    >
+    <div className="headlineBox">
       <em>Some sort of passed in data will go here</em>
     </div>
   );
