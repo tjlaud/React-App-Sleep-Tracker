5db570d7c5a7f9d940993d316ae045223d6bacfc
tjlaud <tjlaud@hotmail.com>
2021-02-27 23:31:12 +0300

    state fail

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,41 +4,86 @@ import Navbar from "../components/Navbar/Navbar";
 import "./App.scss";
 import styles from "./App.module.scss";
 
-function Home() {
-  return (
-    <Box>
-      <Box className="homeBox">
-        <Button
-          variant="contained"
-          color="primary"
-          classes={{ containedPrimary: styles.containedButtonDeepBlue }}
-        >
-          Sleepy Time
-        </Button>
-        <br />
-        <TextField
-          label="Set Time"
-          type="time"
-          defaultValue="07:30"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          inputProps={{
-            step: 300, // 5 min...this doesn't seem to be working as expected
-          }}
-        />
-        <br />
-        <Button
-          variant="contained"
-          color="primary"
-          classes={{ containedPrimary: styles.containedButtonDeepBlue }}
-        >
-          Undo
-        </Button>
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      containedPrimary: styles.containedButtonDeepBlue,
+    };
+  }
+
+  render() {
+    return (
+      <Box>
+        <Box className="homeBox">
+          <Button
+            variant="contained"
+            color="primary"
+            classes={this.state}
+            onClick={() => {
+              // handleChange function to switch states
+            }}
+          >
+            Sleepy Time
+          </Button>
+          <br />
+          <TextField
+            label="Set Time"
+            type="time"
+            defaultValue="07:30"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            inputProps={{
+              step: 300, // 5 min...this doesn't seem to be working as expected
+            }}
+          />
+          <br />
+          <Button
+            variant="contained"
+            color="primary"
+            classes={{ containedPrimary: styles.containedButtonDeepBlue }}
+          >
+            Undo
+          </Button>
+          <br />
+          <Button
+            variant="contained"
+            color="primary"
+            classes={{
+              containedPrimary: styles.containedButtonDeepBlue,
+              containedPrimary: styles.hidden, // this is temporary
+            }}
+            onClick={() => {
+              // set state here to "nightMode"
+            }}
+          >
+            Wake Up
+          </Button>
+        </Box>
+        <Navbar />
       </Box>
-      <Navbar />
-    </Box>
-  );
+    );
+  }
 }
 
 export default Home;
+
+// JS with a switch between day and night. When the button is clicked an onchange sends it to this function
+// class Test extends React.Component {
+//   constructor() {
+//     dayMode = false
+//     if dayMode ?!
+//     };
+// }
+
+// the div then returns a classes with (this.state)
+// render() {
+//     return (
+//         <div>
+//           <p>{this.state.id}</p>
+//           <p>{this.state.name}</p>
+//         </div>
+//     );
+// }
+// }
