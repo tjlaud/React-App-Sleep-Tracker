a8bd658c1bf7f56e2ccca5ab34bf340af02b3408
Alun Turner <alun.turner@googlemail.com>
2021-03-06 10:30:08 +0000

    suggestion for header/navbar

--- src/App.js
+++ src/App.js
@@ -11,9 +11,20 @@ import CreateDet from "./pages/CreateDet";
 import ManageDet from "./pages/ManageDet";
 import { DummyDataContext, dummyData } from "./data/index";
 import "./App.scss";
+/* Hi Tom, this is going to explain how I propose we get the header and navbar to remain on the page throughout. Right, so below when we render the App component, all
+we're doing is rendering a router at the moment. That's fine and is pretty close to what we want. 
+
+To make it exactly what we want, what we want the App component to do is render a Header component, then the Router (which can display _anything_ we've already built), then a navbar. I've annotated this in the return statement below, prefixed by  <<<.
+
+Then this means that when the user changes url, the header and navbar will stay where they are, only the router stuff will change.
+
+This is not as simple as it sounds and will involved quite a bit of jiggery pokery to get the relevant parts of state in the right place (this component will likely have to start storing some state, for example).
+
+Doing this will probably be quite challenging and definitely improve your understanding of React, don't be disheartened if progress is slow to start off with.*/
 
 function App() {
   return (
+    // <<< we can put a <Header /> component in here
     <DummyDataContext.Provider value={dummyData}>
       <Router className="routerBox">
         <Login path="/login" default />
@@ -26,6 +37,7 @@ function App() {
         <Summary path="/summary" />
       </Router>
     </DummyDataContext.Provider>
+    // <<< we can put a <Navbar /> component in here
   );
 }
 
